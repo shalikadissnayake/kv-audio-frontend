97cf6693dccf0e354925ca740486d710f1209abb
shali dissanayaka <dissshalimmw25@gmail.com>
2025-01-28 18:56:40 +0530

    init💻✔

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <>
+       <ProductCard name ="Audio jbl" price="2500/-" description="uyrfitiutiutiyi ugug" img="https://tse1.mm.bing.net/th?id=OIP.msOiNCNpSkLjuwULS8__sgHaE7&pid=Api&P=0&h=220"/>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,14 @@
+import "./productCard.css";
+export default function ProductCard(props){
+
+    console.log(props)
+return(
+    <div>
+       <img src={props.img}/>
+       <span>{props.name}</span>
+       <span>LKR{props.price}</span>
+       <p>{props.description}</p>
+        
+    </div>
+)
+}
