d5b1df5bb584847cef342993a031bdeee8c8552b
shali dissanayaka <dissshalimmw25@gmail.com>
2025-02-03 23:02:52 +0530

    Normalize line endings

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ function App() {
   
 
   return (
-    <>
-       <ProductCard name ="Audio jbl" price="2500/-" description="uyrfitiutiutiyi ugug" img="https://tse1.mm.bing.net/th?id=OIP.msOiNCNpSkLjuwULS8__sgHaE7&pid=Api&P=0&h=220"/>
-       
-    </>
+<div className='bg-red-500'>
+              hfgjygkgkttjgkhjl,jh
+</div>
   )
 }
 
