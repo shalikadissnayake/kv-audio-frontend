59163624d46770fdc0b277b2af9d464c3f338e4e
shali dissanayaka <dissshalimmw25@gmail.com>
2025-02-05 22:19:47 +0530

    day 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 
 import './App.css'
 import ProductCard from './components/productCard'
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark } from "react-icons/fa";
+import { MdOutlineSpeaker } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+
 
 function App() {
   
 
   return (
-<div className='bg-red-500'>
-              hfgjygkgkttjgkhjl,jh
-</div>
+  <div className='w-full h-screen flex'>
+    <div className='w-[400px] h-full bg-green-200'>
+      <button className='w-full h-[40px] text-[25px] font-bold  flex justify-center items-center'>
+        <BsGraphDown/>
+         Dashboard
+      </button>
+      <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+        <FaRegBookmark/>
+        Bookings
+      </button>
+       <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+        <MdOutlineSpeaker/>
+        Items
+      </button>
+       <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+        <FaRegUser/>
+        Users
+      </button>
+
+    </div>
+    <div className='w-full bg-red-600'>
+    
+
+    </div>
+ 
+  </div>
+
   )
 }
 
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import react from '@vitejs/plugin-react';
+
+export default {
+  plugins: [react()],
+};
+
+
+
+
