42438c93e04ac31470eb59e101c534e729ee206d
shali dissanayaka <dissshalimmw25@gmail.com>
2025-02-10 21:53:38 +0530

    day 1o admin page component✔✅

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,15 @@ import { BsGraphDown } from "react-icons/bs";
 import { FaRegBookmark } from "react-icons/fa";
 import { MdOutlineSpeaker } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa";
+import AdminPage from './components/adminPage';
 
 
 function App() {
   
 
   return (
-  <div className='w-full h-screen flex'>
-    <div className='w-[400px] h-full bg-green-200'>
-      <button className='w-full h-[40px] text-[25px] font-bold  flex justify-center items-center'>
-        <BsGraphDown/>
-         Dashboard
-      </button>
-      <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
-        <FaRegBookmark/>
-        Bookings
-      </button>
-       <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
-        <MdOutlineSpeaker/>
-        Items
-      </button>
-       <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
-        <FaRegUser/>
-        Users
-      </button>
-
-    </div>
-    <div className='w-full bg-red-600'>
-    
-
-    </div>
- 
+  <div>
+    <AdminPage/>
   </div>
 
   )
