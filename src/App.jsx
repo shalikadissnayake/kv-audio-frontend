e030b9cdd889bcf43a3694eee5880af0a448a391
shali dissanayaka <dissshalimmw25@gmail.com>
2025-02-13 22:15:17 +0530

    day 12 complete✅

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import ProductCard from './components/productCard'
 import AdminPage from './pages/admin/adminPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/homePage';
+import Testing from './components/testing';
+import LoginPage from './pages/login/login';
+import { Toaster } from 'react-hot-toast';
 
 
 function App() {
@@ -12,7 +15,10 @@ function App() {
 
   return (
   <BrowserRouter>
+  <Toaster position='top-right'/>
   <Routes path="/*">
+     <Route path="/testing" element={<Testing/>}/>
+     <Route path="/login" element={<LoginPage/>}/>
      <Route path="/admin/*" element={<AdminPage/>}/>
      <Route path="/*" element={<HomePage/>}/>
      
