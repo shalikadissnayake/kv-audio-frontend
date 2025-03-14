e4db828f3f304348ff4cd19f0058be6df8b66d5b
shali dissanayaka <dissshalimmw25@gmail.com>
2025-03-14 11:53:11 +0530

    17 day re-edit 👌✔

--- src/pages/home/items.jsx
+++ src/pages/home/items.jsx
@@ -20,7 +20,7 @@ export default function Items(){
       
     },[])
     return(
-        <div className="w-full h-full flex flex-wrap justify-center  pt-[50px]">
+        <div className="  w-full h-full flex flex-wrap justify-center  pt-[50px]">
             {
                 state=="loading"&& 
                 <div className="w-full h-full  flex justify-center items-center">
